b6c7532f383682c4e3a03ae4398e895458875f2b
airenas <airenass@gmail.com>
2022-07-21 09:33:50 +0300

    Fix log

--- extractor.py
+++ extractor.py
@@ -113,8 +113,7 @@ def get_videos(url):
 def extract_date(p_html):
     m = p_html.find('span', attrs={'class': 'info-block__text'})
     if m:
-        logging.info('Date ')
-        logging.info(m.get_text().encode('utf-8'))
+        logging.info('Date {0}'.format(m.get_text().encode('utf-8')))
         return m.get_text()
     logging.info('No date ')
     return None
